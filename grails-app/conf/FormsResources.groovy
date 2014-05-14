733747e2c9d67d2e1046d05844c506871ae86b09
Joost Horward <joost@open-t.nl>
2014-05-14 20:47:17 +0200

    Removed dialog-last dependency from xml-forms resource

--- grails-app/conf/FormsResources.groovy
+++ grails-app/conf/FormsResources.groovy
@@ -1,17 +1,17 @@
 modules = {
 	'xml-forms' {
-		dependsOn 'dialog,dialog-dataTables,dialog-altselect,dialog-ckeditor,dialog-codemirror,bootstrap-css,bootstrap-tooltip,bootstrap-popover,bootstrap-modal,dialog-last'
+		dependsOn 'dialog,dialog-dataTables,dialog-altselect,dialog-ckeditor,dialog-codemirror,bootstrap-css,bootstrap-tooltip,bootstrap-popover,bootstrap-modal'
 		resource url:'js/xml-forms.js'
 		resource url:'css/xml-forms.css'
 		resource url:'js/jquery/jquery.validate.pack.js'
 	}
-	
+
 	'xml-forms-datepicker' {
         dependsOn 'xml-forms'
-		resource url:'js/jquery/jquery-ui-timepicker-addon.js'		
+		resource url:'js/jquery/jquery-ui-timepicker-addon.js'
 		resource url:'js/xml-forms.datepicker.js'
 	}
-    
+
     'xml-forms-tabs' {
         dependsOn 'xml-forms'
 		resource url:'js/xml-forms.tabs.js'
